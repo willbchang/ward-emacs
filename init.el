39df244dca39e37ed0e63e443e727faffe617bed
Will B Chang <will.b.chang@gmail.com>
2020-07-16 16:44:38 +0800

    refactor: move and split code with Font, Color Theme, Text Editing, Window Management, Behavior and Proxy

--- init.el
+++ init.el
@@ -190,11 +190,12 @@
 
 ;; The code above will be refactored in the future.
 ;; ------------------- Customized Configuation ----------------------
-;; ------------------- UI -------------------------------------------
+;; ------------------- Font -------------------------------------------
 ;; Set Font Family and Font Size
 (set-face-attribute 'default nil
                     :font "Inconsolata-18")
 
+;; ------------------- Color Theme --------------------------------------
 ;; Set Color Theme
 (load-theme 'idea-darkula t)
 
@@ -206,7 +207,7 @@
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
 
 
-;; ------------------- Shortcuts ------------------------------------
+;; ------------------- Text Editing --------------------------------------
 ;; Set Command + ArrowKey to behave like MacOS
 (global-set-key (kbd "s-<up>") 'beginning-of-buffer)
 (global-set-key (kbd "s-<down>") 'end-of-buffer)
@@ -217,35 +218,15 @@
 (global-set-key (kbd "s-<backspace>") 'backward-kill-line)
 ;; Kill whole line entirely
 (global-set-key (kbd "s-S-<backspace>") 'kill-whole-line)
-;; Close current buffer
-(global-set-key (kbd "s-w") 'kill-this-buffer)
 
 ;; Set shortcut to comment/uncomment a region/line
 (global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
-
-;; Set frame full screen with Ctrl + Command + F
-(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
-;; Create new frame with Command + N, default with org-mode and evil-mode
-(global-set-key (kbd "s-n") 'new-empty-frame)
-;; Close current frame with Shift + Command + W
-(global-set-key (kbd "s-W") 'delete-frame)
 
 
 (defun backward-kill-line (arg)
   "Kill ARG lines backward."
   (interactive "p")
-  (kill-line (- 1 arg)))
-
-;; TODO: Make the buffers independent in each frame.
-(defun new-empty-frame ()
-  "Create a new frame with a new empty buffer. With org-mode and evil-mode enabled."
-  (interactive)
-  (let ((buffer (generate-new-buffer "untitled")))
-    (set-buffer buffer)
-    (org-mode)
-    (evil-mode 1)
-    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
-
+ (kill-line (- 1 arg)))
 ;; TODO:
 ;; 1. Comment on empty line, it adds ;; (e.g.) and put the cursor behind
 ;; 2. Comment one line, it adds ;; before and forward one line
@@ -259,6 +240,28 @@ there's no active region."
         (setq beg (region-beginning) end (region-end))
       (setq beg (line-beginning-position) end (line-end-position)))
     (comment-or-uncomment-region beg end)))
+
+
+;; ------------------- Window Management ---------------------------------
+;; Set frame full screen with Ctrl + Command + F
+(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
+;; Create new frame with Command + N, default with org-mode and evil-mode
+(global-set-key (kbd "s-n") 'new-empty-frame)
+
+;; Close current buffer
+(global-set-key (kbd "s-w") 'kill-this-buffer)
+;; Close current frame with Shift + Command + W
+(global-set-key (kbd "s-W") 'delete-frame)
+
+;; TODO: Make the buffers independent in each frame.
+(defun new-empty-frame ()
+  "Create a new frame with a new empty buffer. With org-mode and evil-mode enabled."
+  (interactive)
+  (let ((buffer (generate-new-buffer "untitled")))
+    (set-buffer buffer)
+    (org-mode)
+    (evil-mode 1)
+    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
 
 
 ;; ------------------- Behaviors ------------------------------------
@@ -284,9 +287,6 @@ there's no active region."
 
 ;; Disable the ring bell when scroll beyond the document
 (setq ring-bell-function 'ignore)
-
-;; ------------------- Functionalities ------------------------------
-
 
 ;; ------------------- Proxy ----------------------------------------
 ;; Start server for opening file/folder from emacsclient
