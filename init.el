e7f0bafd6404b827af7adc29ad2c7ffdfda1ca52
Will B Chang <will.b.chang@gmail.com>
2020-07-17 08:46:57 +0800

    chore: auto add content by use-package

--- init.el
+++ init.el
@@ -16,3 +16,15 @@
 
 (org-babel-load-file "~/.emacs.d/README.org")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil use-package idea-darkula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
