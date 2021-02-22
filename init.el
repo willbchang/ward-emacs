b7e3295dc1f70e43fe16e57f619a69bc54d61661
Will B Chang <will.b.chang@gmail.com>
2021-02-22 09:04:08 +0800

    terminal: add exec-path-from-shell to make the enviroment variables in shell works in vterm

--- init.el
+++ init.el
@@ -21,11 +21,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil use-package idea-darkula-theme))))
+ '(package-selected-packages
+    '(exec-path-from-shell vterm evil use-package idea-darkula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:weight normal))))
-)
+ '(mode-line ((t (:weight normal)))))
