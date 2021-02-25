947d859b4579996c91e682425be1e02b25a0175a
Will B Chang <will.b.chang@gmail.com>
2021-02-25 09:33:03 +0800

    package: add markdown-mode

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-    '(exec-path-from-shell vterm evil use-package idea-darkula-theme)))
+   '(markdown-mode exec-path-from-shell vterm evil use-package idea-darkula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
