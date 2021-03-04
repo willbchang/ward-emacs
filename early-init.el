bbbea16a866222b34e7bf50473b8e2cb8dccfe05
Will B Chang <will.b.chang@gmail.com>
2021-03-04 15:59:38 +0800

    early-init: set library path
    Make libgccjit able to use gcc which installed from homebrew

---
+++ early-init.el
@@ -0,0 +1,2 @@
+;; Make libgccjit able to use gcc which installed from homebrew
+(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
