082e75a0667bec8d1cf0679df679b05325446a67
Will B Chang <will.b.chang@gmail.com>
2021-03-07 10:57:11 +0800

    init: fix typo

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
   '(evil-pinyin jetbrains-darcula-theme valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package)))
-(custom-set-fac
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
