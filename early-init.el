d0e5028a32466bdd77d69d2b768d239b4252cf2e
Will B Chang <will.b.chang@gmail.com>
2021-03-08 12:04:58 +0800

    use-package: add link and description

--- early-init.el
+++ early-init.el
@@ -1,2 +1,4 @@
 ;; Make libgccjit able to use gcc which installed from homebrew
 (setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
+
+(setq warning-minimum-level :error)
--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-  '(evil-pinyin jetbrains-darcula-theme valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package)))
+   '(evil-pinyin jetbrains-darcula-theme valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
