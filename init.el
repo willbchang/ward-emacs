0d4cd4f2202fd6a7f03493b79211cff7060ca918
Will B Chang <will.b.chang@gmail.com>
2021-03-15 10:15:06 +0800

    chinese: use sis to replace evil-switch-input-method

--- init.el
+++ init.el
@@ -6,8 +6,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-line-numbers-type 'relative)
+ '(git-gutter:update-interval 2)
  '(package-selected-packages
-   '(auto-package-update evil-pinyin jetbrains-darcula-theme valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package)))
+   '(sis auto-package-update evil-pinyin jetbrains-darcula-theme valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
