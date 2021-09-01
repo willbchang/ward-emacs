4fb5fceaaae599b29c0d83fac9de75a5136fbac5
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-01 13:31:07 +0800

    early-init: load basic keybindings on start
      This way I will be able to use the macOS like keybindings even if
      Emacs has error in init file

--- early-init.el
+++ early-init.el
@@ -12,19 +12,80 @@
 (setq package-enable-at-startup nil)
 (setq package-quickstart t)
 
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-;; Faster to disable these here (before they've been initialized)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+;; Make libgccjit able to use gcc which installed from homebrew
+;;(setenv "LIBGCCJITIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
+
+;;(setq warning-minimum-level :error)
+
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
 (setq frame-inhibit-implied-resize t)
 
-;; Make libgccjit able to use gcc which installed from homebrew
-;;(setenv "LIBGCCJITIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
 
-;;(setq warning-minimum-level :error)
+
+;;-------------------------Key Bindings----------------------------------------
+;; Make the macOS like keybinding work even Emacs's init file has bug, so I don't need to use the default keybinding which makes me like a dumb.
+;; Make Option and Command work normal in Emacs Mac Port.
+(setq mac-option-key-is-meta t
+      x-select-enable-clipboard 't
+      mac-command-modifier 'super
+      mac-option-modifier 'meta)
+
+;; Frame shortcuts
+(global-set-key (kbd "s-q") 'save-buffers-kill-emacs)
+(global-set-key (kbd "s-W") 'delete-frame)
+(global-set-key (kbd "s-`") 'other-frame)
+(global-set-key (kbd "M-`") 'other-window)
+(global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
+
+;; Buffer shortcuts
+(global-set-key (kbd "s-w") 'kill-buffer-and-window)
+(global-set-key (kbd "s-[") 'previous-buffer)
+(global-set-key (kbd "s-]") 'next-buffer)
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-,") 'open-config-file)
+
+(defun open-config-file ()
+  (interactive)
+  (find-file "~/.config/emacs/config.org"))
+
+(defun reload-init-file ()
+  (interactive)
+  (load-file user-init-file))
+
+;; Moving Cursor
+(global-set-key (kbd "s-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "s-<down>") 'end-of-buffer)
+(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+(global-set-key (kbd "s-<right>") 'move-end-of-line)
+
+;; Selecting Text
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
+
+;; Editing Text
+(global-set-key (kbd "s-c") 'kill-ring-save)
+(global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-z") 'undo)
+(global-set-key (kbd "<s-return>") 'newline)
+(global-set-key (kbd "s-<backspace>") 'backward-kill-line)
+(global-set-key (kbd "s-S-<backspace>") 'kill-whole-line)
+(global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
+
+(defun backward-kill-line (arg)
+  "Kill ARG lines backward."
+  (interactive "p")
+  (kill-line (- 1 arg)))
+
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if
+there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
 
