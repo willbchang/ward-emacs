7b5e94610fc3a3acef6bb643ddc9417d0e6269be
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-02 22:47:35 +0800

    theme: change modeline and term color

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dired magit org-superstar evil-surround undo-fu-session flx counsel auto-compile amx ace-pinyin avy org-download restart-emacs gitignore-mode gitconfig-mode gitattributes-mode ivy-rich diff-hl evil-snipe ivy-prescient ivy org-appear sis evil-pinyin valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package))
+   '(vterm-toggle dired magit org-superstar evil-surround undo-fu-session flx counsel auto-compile amx ace-pinyin avy org-download restart-emacs gitignore-mode gitconfig-mode gitattributes-mode ivy-rich diff-hl evil-snipe ivy-prescient ivy org-appear sis evil-pinyin valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -16,5 +16,8 @@
  '(diff-hl-delete ((t (:background "#f5cce1"))))
  '(diff-hl-insert ((t (:background "#80f1a4"))))
  '(fringe ((t nil)))
- '(mode-line ((t (:underline t :height 0.1))))
- '(mode-line-inactive ((t (:inherit mode-line)))))
+ '(mode-line ((t (:foreground "dim gray" :underline t :height 0.1))))
+ '(mode-line-inactive ((t (:inherit mode-line))))
+ '(term-color-blue ((t (:background "blue2" :foreground "dodger blue"))))
+ '(term-color-red ((t (:background "red3" :foreground "red"))))
+ '(term-color-yellow ((t (:background "yellow3" :foreground "orange")))))
