046c58b8de19c13d76eb008c44091913c225caf6
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-11 16:54:59 +0800

    ui: enhance elisp code highlight

--- custom.el
+++ custom.el
@@ -4,9 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm-toggle dired magit org-superstar evil-surround undo-fu-session flx counsel auto-compile amx ace-pinyin avy org-download restart-emacs gitignore-mode gitconfig-mode gitattributes-mode ivy-rich diff-hl evil-snipe ivy-prescient ivy org-appear sis evil-pinyin valign undo-fu markdown-mode exec-path-from-shell vterm evil use-package))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
+   '(highlight-defined which-key vterm-toggle valign use-package undo-fu-session undo-fu spacemacs-theme sis restart-emacs quelpa org-superstar org-appear markdown-mode magit ivy-rich gitignore-mode gitconfig-mode gitattributes-mode flx find-file-in-project exec-path-from-shell evil-surround evil-snipe evil-pinyin evil-collection diff-hl counsel auto-compile amx ace-pinyin)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
