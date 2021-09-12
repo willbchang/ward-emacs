36c462eab19747e30e03644bd178de7aa33a6b84
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-13 07:36:31 +0800

    package: upgrade

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(highlight-defined which-key vterm-toggle valign use-package undo-fu-session undo-fu spacemacs-theme sis restart-emacs quelpa org-superstar org-appear markdown-mode magit ivy-rich gitignore-mode gitconfig-mode gitattributes-mode flx find-file-in-project exec-path-from-shell evil-surround evil-snipe evil-pinyin evil-collection diff-hl counsel auto-compile amx ace-pinyin)))
+   '(vterm highlight-defined which-key valign use-package undo-fu-session undo-fu spacemacs-theme sis restart-emacs quelpa org-superstar org-appear markdown-mode magit ivy-rich gitignore-mode gitconfig-mode gitattributes-mode flx find-file-in-project exec-path-from-shell evil-surround evil-snipe evil-pinyin evil-collection diff-hl counsel auto-compile amx ace-pinyin)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
