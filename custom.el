3e56b7a55b7b80b9e8d1e296d015f4efc391807e
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-13 09:29:25 +0800

    modeline: move custom face to config

--- custom.el
+++ custom.el
@@ -10,13 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diff-hl-change ((t (:background "#8fe9e3"))))
- '(diff-hl-delete ((t (:background "#f5cce1"))))
- '(diff-hl-insert ((t (:background "#80f1a4"))))
  '(fringe ((t nil)))
- '(header-line ((t (:background "grey90" :foreground "grey20" :box nil))))
- '(mode-line ((t (:foreground "dim gray" :underline t :height 0.1))))
- '(mode-line-inactive ((t (:inherit mode-line))))
  '(term-color-blue ((t (:background "blue2" :foreground "dodger blue"))))
  '(term-color-red ((t (:background "red3" :foreground "red"))))
  '(term-color-yellow ((t (:background "yellow3" :foreground "orange")))))
