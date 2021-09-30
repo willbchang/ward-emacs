e634179be7765cf8648feaa9b3423b0be5a56dd5
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-09-30 13:23:16 +0800

    custom: update

--- custom.el
+++ custom.el
@@ -4,13 +4,26 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm highlight-defined which-key valign use-package undo-fu-session undo-fu spacemacs-theme sis restart-emacs quelpa org-superstar org-appear markdown-mode magit ivy-rich gitignore-mode gitconfig-mode gitattributes-mode flx find-file-in-project exec-path-from-shell evil-surround evil-snipe evil-pinyin evil-collection diff-hl counsel auto-compile amx ace-pinyin)))
+   '(evil-goggles with-editor vterm highlight-defined which-key valign use-package undo-fu-session undo-fu spacemacs-theme sis restart-emacs quelpa org-superstar org-appear markdown-mode magit ivy-rich gitignore-mode gitconfig-mode gitattributes-mode flx find-file-in-project exec-path-from-shell evil-surround evil-snipe evil-pinyin evil-collection diff-hl counsel auto-compile amx ace-pinyin)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:background "#8fe9e3"))))
+ '(diff-hl-delete ((t (:background "#f5cce1"))))
+ '(diff-hl-insert ((t (:background "#80f1a4"))))
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed))))
  '(fringe ((t nil)))
+ '(header-line ((t (:background "grey90" :foreground "grey20" :box nil))))
+ '(mode-line ((t (:foreground "dim gray" :underline t :height 0.1))))
+ '(mode-line-inactive ((t (:inherit mode-line))))
  '(term-color-blue ((t (:background "blue2" :foreground "dodger blue"))))
  '(term-color-red ((t (:background "red3" :foreground "red"))))
  '(term-color-yellow ((t (:background "yellow3" :foreground "orange")))))
