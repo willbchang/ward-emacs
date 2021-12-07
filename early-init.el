ce8510b26f0661f41cdbabe51699b8eaf770e1a7
Will B Chang <14329786+willbchang@users.noreply.github.com>
2021-12-07 09:09:36 +0800

    early-init: setup library path for libgccgit

--- early-init.el
+++ early-init.el
@@ -31,7 +31,7 @@
 (setq package-quickstart t)
 
 ;; Make libgccjit able to use gcc which installed from homebrew
-;;(setenv "LIBGCCJITIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/10:/usr/local/opt/gcc/lib/gcc/10/gcc/x86_64-apple-darwin20/10.2.0")
+;; (setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/11:/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin21/11")
 
 ;;(setq warning-minimum-level :error)
 
