dfdbd4e961617c38e70276bd41c169b82119968c
Will B Chang <14329786+willbchang@users.noreply.github.com>
2022-02-22 23:01:28 +0800

    early-init: smarter move beginning of line

--- early-init.el
+++ early-init.el
@@ -89,9 +89,33 @@
   (load-file user-init-file))
 
 ;; Moving Cursor
+;; https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
 (global-set-key (kbd "s-<up>") 'beginning-of-buffer)
 (global-set-key (kbd "s-<down>") 'end-of-buffer)
-(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+(global-set-key (kbd "s-<left>") 'smarter-move-beginning-of-line)
 (global-set-key (kbd "s-<right>") 'move-end-of-line)
 
 ;; Selecting Text
