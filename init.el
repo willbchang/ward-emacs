cc6d62bd167a4d62e6e72024c3d43672e004bc1b
Will B Chang <14329786+willbchang@users.noreply.github.com>
2022-03-01 10:19:06 +0800

    init: move suppress message to config

--- init.el
+++ init.el
@@ -1,19 +1,4 @@
 ;;; -*- lexical-binding: t; -*-
-;; Forces the messages to 0, and kills the *Messages* buffer - thus disabling it on startup.
-;; (kill-buffer "*Messages*")
-
-;; Disable unhelpful mesages in minibuffer.
-;; https://superuser.com/a/1025827/1114552
-;; https://www.reddit.com/r/emacs/comments/df3kko/suppress_some_message_in_minibuffer/
-
-(defun suppress-messages (func &rest args)
-  (cl-letf (((symbol-function 'message)
-              (lambda (&rest args) nil)))
-     (apply func args)))
-
-(advice-add 'load :around #'suppress-messages)
-(advice-add 'org-babel-load-file :around #'suppress-messages)
-(advice-add 'write-region :around #'suppress-messages)
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (set 'ad-redefinition-action 'accept)
