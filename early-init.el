deece16e86a77d4b448b1aa7bfcb608452bbdcb4
Will B Chang <14329786+willbchang@users.noreply.github.com>
2022-05-26 22:55:32 +0800

    early-init: remove gc and file-name-handler-alist settings
      file-name-handler-alist make
      find-function--search-by-expanding-macros unusable in help-mode

--- early-init.el
+++ early-init.el
@@ -1,13 +1,7 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; Emacs (27+) introduces early-init.el, which is run before init.el,
 ;; before package and UI initialization happens.
-;; PATCH: UX
-(setq file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6
-      auto-window-vscroll nil)
 
 ;; In Emacs 27+, package initialization occurs before `user-init-file' is
 ;; loaded, but after `early-init-file'. Doom handles package initialization, so
